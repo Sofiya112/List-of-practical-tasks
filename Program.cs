44477e43e84fef36cb7e1deb02d0aedbd104e1f4
Kab-35-16 <Kab-35-16@KAB-31-3->
2025-05-29 11:11:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Введите первое число: ");
+            double side1 = Convert.ToDouble( Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            double side2 = Convert.ToDouble( Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Rectangle
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+
+        
+        public Rectangle(double side1, double side2) // Конструктор с параметрами
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+
+        
+        public double PerimeterCalculator() //Периметр
+        {
+            return 2 * (Side1 + Side2);
+        }
+
+        
+        public double AreaCalculator() // Площадь
+        {
+            return Side1 * Side2;  
+        }
+
+        public double Perimeter => PerimeterCalculator();
+        public double Area => AreaCalculator();
+    }
+}
